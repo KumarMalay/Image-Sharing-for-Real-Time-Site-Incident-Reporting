d385cdc5443bf66284250e4d6ee0c8f2722fb5a2
KumarMalay <malayprasad709@gmail.com>
2023-09-12 02:05:00 +0530

    Completed

--- final_app/lib/main.dart
+++ final_app/lib/main.dart
@@ -21,9 +21,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      // theme: ThemeData(fontFamily: "MontaguSlab"),
       home: AuthPageSql(),
-      // home: CameraScreen(),
     );
   }
 }
